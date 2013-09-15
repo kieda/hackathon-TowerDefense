e3a220573901d0709a3811a3001696e20ed5d3eb
shaanvaria <shaanvaria@192.168.8.109>
2013-09-14 20:45:25 -0400

    made square class

--- TowerDefense/src/mechanics/Square.java
+++ TowerDefense/src/mechanics/Square.java
@@ -9,5 +9,36 @@ package mechanics;
  * @author Shaan
  */
 public class Square {
+
+    private int x;
+    private int y;
+    private boolean occupied;
+
+    public Square(int xco, int yco) {
+        this.x = xco;
+        this.y = yco;
+    }
+    
+    public Square(int xco, int yco, boolean occupied){
+        this.x = xco;
+        this.y = yco;
+        this.occupied = occupied;
+    }
+    
+    public int getX(){
+        return this.x;
+    }
+    
+    public int getY(){
+        return this.y;
+    }
+    
+    public void setOccupied(boolean occupied){
+        this.occupied=occupied;
+    }
+    
+    public boolean getOccupied(){
+        return this.occupied;
+    }
     
 }
